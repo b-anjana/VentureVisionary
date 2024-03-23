17ad316f309e6c4a9891e14306f2f73c342cc008
sanyavashishth9 <savashishth@gmail.com>
2024-03-23 18:56:48 -0500

    added google map interface

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+"use client";
 
-function App() {
+import './App.css';
+import { APIProvider } from '@vis.gl/react-google-maps';
+import { useState } from "react";
+import {
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+} from "@vis.gl/react-google-maps";
+
+export default function App() {
+  const position = { lat: 53.54, lng: 10 };
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <APIProvider apiKey='AIzaSyB9QU0vQ9nhrMgwL8VCCnFh2d0dRm0uLJs'>
+      <div style={{height: "100vh", width: "75vw"}}>
+        <Map 
+          zoom={9}  
+          center={position} 
+          mapId = {"348ffe81b5913ac0"}
+        > 
+          <AdvancedMarker 
+            position={position}
+            onClick={() => setOpen(true)}>
+          </AdvancedMarker>
+          {open && <InfoWindow position={position}><p>I'm in hell</p></InfoWindow>}
+        </Map>
+      </div>
+    </APIProvider>
+
   );
 }
 
-export default App;
+//export default App;
