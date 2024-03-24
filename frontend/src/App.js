b15da3aae0b7dbd565a004e900ab7d1eea710b80
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2024-03-23 21:40:56 -0500

    create frontend and backend connect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
-"use client";
-
+//import logo from './logo.svg';
 import './App.css';
-import MapShow from './Map'
+import MapShow from './Map';
+import LLM from './LLM';
 
 export default function App() {
   return (
-    <MapShow />
+    <>
+      <MapShow />
+      <LLM />
+    </>
   );
 }
   
